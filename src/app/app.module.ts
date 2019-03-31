9f55f53e0394852e283256666fb3337d829ab9a7
dibosh <dibosh@newscred.com>
2019-04-01 01:16:07 +0800

    Create the basic tag input component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TagInputComponent } from './tag-input/tag-input.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TagInputComponent
   ],
   imports: [
     BrowserModule,
