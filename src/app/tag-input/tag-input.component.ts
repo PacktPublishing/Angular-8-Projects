fbf0cf6c5acee838ed954f65419ab8672f5a0c34
dibosh <dibosh@newscred.com>
2019-04-01 01:35:57 +0800

    Complete implementation of tag input

--- src/app/tag-input/tag-input.component.ts
+++ src/app/tag-input/tag-input.component.ts
@@ -6,11 +6,23 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./tag-input.component.scss']
 })
 export class TagInputComponent implements OnInit {
+  inputText: string;
   tags: string[] = [];
   hint: string;
 
   constructor() {
     this.hint = 'Try some comma separated words';
+  }
+
+  onKey(event: any) {
+    if (event.key === 'Enter') {
+      this.tags = this.inputText.trim().split(',').map(val => val.trim());
+      this.inputText = '';
+    }
+  }
+
+  onDelete(index: number) {
+    this.tags.splice(index, 1);
   }
 
   ngOnInit() {
