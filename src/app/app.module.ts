d3bcb5cdd6b9fab453d7af350c2fc69e7d447aad
dibosh <dibosh@newscred.com>
2019-04-01 00:44:26 +0800

    Use auto correct input component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {AutoCorrectInputComponent} from './auto-correct-input/auto-correct-input.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AutoCorrectInputComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
