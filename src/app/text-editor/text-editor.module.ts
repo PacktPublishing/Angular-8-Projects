140c7264d706b31f112e21f2219aee3badaa17b3
dibosh <dibosh@newscred.com>
2019-05-21 02:03:14 +0800

    Add the main container component

--- src/app/text-editor/text-editor.module.ts
+++ src/app/text-editor/text-editor.module.ts
@@ -2,17 +2,19 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {EditorComponent} from './editor/editor.component';
 import {ToolbarComponent} from './toolbar/toolbar.component';
+import {TextEditorComponent} from './text-editor/text-editor.component';
 
 @NgModule({
   declarations: [
     EditorComponent,
-    ToolbarComponent
+    ToolbarComponent,
+    TextEditorComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    EditorComponent
+    TextEditorComponent
   ]
 })
 export class TextEditorModule {
