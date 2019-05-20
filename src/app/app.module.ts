5b8d60226aa62c75f6392a09a9fb3a1be154c2ff
dibosh <dibosh@newscred.com>
2019-05-21 01:49:11 +0800

    Add text editor module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {TextEditorModule} from './text-editor/text-editor.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TextEditorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
