25c4f9f98c49f3916df65c8f6d000a15f1d14bf9
dibosh <dibosh@newscred.com>
2019-05-21 01:58:43 +0800

    Add toolbar component

--- src/app/text-editor/text-editor.module.ts
+++ src/app/text-editor/text-editor.module.ts
@@ -1,9 +1,13 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {EditorComponent} from './editor/editor.component';
+import {ToolbarComponent} from './toolbar/toolbar.component';
 
 @NgModule({
-  declarations: [EditorComponent],
+  declarations: [
+    EditorComponent,
+    ToolbarComponent
+  ],
   imports: [
     CommonModule
   ],
