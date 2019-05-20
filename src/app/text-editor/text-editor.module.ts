e3e267fcb7bc2cce1d6931255e7ce1d66761bbf5
dibosh <dibosh@newscred.com>
2019-05-21 01:52:48 +0800

    Add editor component

--- src/app/text-editor/text-editor.module.ts
+++ src/app/text-editor/text-editor.module.ts
@@ -1,10 +1,15 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {EditorComponent} from './editor/editor.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [EditorComponent],
   imports: [
     CommonModule
+  ],
+  exports: [
+    EditorComponent
   ]
 })
-export class TextEditorModule { }
+export class TextEditorModule {
+}
